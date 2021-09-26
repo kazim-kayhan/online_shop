95a8061051eafe148f2f970762531b4974b7ba3e
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2021-09-26 21:40:00 +0430

    Add category options

--- resources/views/livewire/admin/admin-edite-category-component.blade.php
+++ resources/views/livewire/admin/admin-edite-category-component.blade.php
@@ -35,6 +35,19 @@
                                 </div>
                             </div>
                             <div class="form-group">
+                                <label for="slug" class="col-md-4 control-label">Parent Category</label>
+                                <div class="col-md-4">
+                                    <select name="" id="" class="form-control" wire:model='category_id'>
+                                        <option value="">None</option>
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                        @endforeach
+
+                                    </select>
+                                    @error('slug') <p class="text-danger">{{ $message }}</p> @enderror
+                                </div>
+                            </div>
+                            <div class="form-group">
                                 <label  class="col-md-4 control-label"></label>
                                 <div class="col-md-4">
                                     <button type="submit" class="btn btn-rimary">Upadate</button>
