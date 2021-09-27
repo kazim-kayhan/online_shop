27746dc6be6421da776cfd77635f4c74d872921f
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2021-09-27 09:54:01 +0430

    Add sucbcate widgets

--- resources/views/livewire/category-component.blade.php
+++ resources/views/livewire/category-component.blade.php
@@ -1,7 +1,5 @@
 <main id="main" class="main-site left-sidebar">
-
     <div class="container">
-
         <div class="wrap-breadcrumb">
             <ul>
                 <li class="item-link"><a href="/" class="link">home</a></li>
@@ -10,21 +8,15 @@
             </ul>
         </div>
         <div class="row">
-
             <div class="col-lg-9 col-md-8 col-sm-8 col-xs-12 main-content-area">
-
                 <div class="banner-shop">
                     <a href="#" class="banner-link">
                         <figure><img src="{{ asset('assets/images/shop-banner.jpg')}}" alt=""></figure>
                     </a>
                 </div>
-
                 <div class="wrap-shop-control">
-
                     <h1 class="shop-title">{{ $category_name }}</h1>
-
                     <div class="wrap-right">
-
                         <div class="sort-item orderby ">
                             <select name="orderby" class="use-chosen" wire:model="sorting">
                                 <option value="menu_order" selected="selected">Default sorting</option>
@@ -33,7 +25,6 @@
                                 <option value="price-desc">Sort by price: high to low</option>
                             </select>
                         </div>
-
                         <div class="sort-item product-per-page">
                             <select name="post-per-page" class="use-chosen" wire:model="pagesize">
                                 <option value="12" selected="selected">12 per page</option>
@@ -50,11 +41,10 @@
                             <a href="list.html" class="list-mode display-mode"><i class="fa fa-th-list"></i>List</a>
                         </div>
                     </div>
-                </div><!--end wrap shop control-->
+                </div>
+                <!--end wrap shop control-->
                 <div class="row">
-                    <ul class="product-list grid-products equal-container">
-                        @foreach ($products as $product)
-                        <li class="col-lg-4 col-md-6 col-sm-6 col-xs-6 ">
+                    <ul class="product-list grid-products equal-container"> @foreach ($products as $product) <li class="col-lg-4 col-md-6 col-sm-6 col-xs-6 ">
                             <div class="product product-style-3 equal-elem ">
                                 <div class="product-thumnail">
                                     <a href="{{ route('product.details',['slug'=>$product->slug]) }}" title="{{ $product->name }}">
@@ -67,35 +57,23 @@
                                     <a href="#" class="btn add-to-cart" wire:click.prevent="store({{ $product->id }},'{{ $product->name }}',{{ $product->regular_price }})">Add To Cart</a>
                                 </div>
                             </div>
-                        </li>
-                        @endforeach
-                    </ul>
+                        </li> @endforeach </ul>
                 </div>
-                <div class="wrap-pagination-info">
-                    {{$products->links()}}
-                    {{-- <ul class="page-numbers">
-                        <li><span class="page-number-item current" >1</span></li>
-                        <li><a class="page-number-item" href="#" >2</a></li>
-                        <li><a class="page-number-item" href="#" >3</a></li>
-                        <li><a class="page-number-item next-link" href="#" >Next</a></li>
-                    </ul>
-                    <p class="result-count">Showing 1-8 of 12 result</p> --}}
-                </div>
-            </div><!--end main products area-->
+                <div class="wrap-pagination-info"> {{$products->links()}} </div>
+            </div>
+            <!--end main products area-->
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 sitebar">
                 <div class="widget mercado-widget categories-widget">
                     <h2 class="widget-title">All Categories</h2>
                     <div class="widget-content">
-                        <ul class="list-category">
-                            @foreach ($categories as $category)
-                            <li class="category-item">
-                                <a href="{{ route('product.category',['category_slug'=>$category->slug]) }}" class="cate-link">{{ $category->name }}</a>
-                            </li>
-                            @endforeach
-                        </ul>
+                        <ul class="list-category"> @foreach ($categories as $category) <li class="category-item" {{ count($category->subCategory)>0 ? 'has-child-cate':'' }}>
+                                <a href="{{ route('product.category', ['category_slug' => $category->slug]) }}" class="cate-link">{{ $category->name }}</a> @if (count($category->subCategory)>0) <span class="toggle-control">+</span>
+                                <ul class="sub-cate"> @foreach ($category->subCategories as $category) <li category-item>
+                                        <a href="{{ route('product.category',['category_slug'=>$category->slug,'scategory_slug'=>$scategory->slug]) }}" class="cat-link"><i class="fa fa-caret-right"></i> {{ $category->name }}</a>
+                                    </li> @endforeach </ul> @endif
+                            </li> @endforeach </ul>
                     </div>
                 </div><!-- Categories widget-->
-
                 <div class="widget mercado-widget filter-widget brand-widget">
                     <h2 class="widget-title">Brand</h2>
                     <div class="widget-content">
@@ -114,7 +92,6 @@
                         </ul>
                     </div>
                 </div><!-- brand widget-->
-
                 <div class="widget mercado-widget filter-widget price-filter">
                     <h2 class="widget-title">Price</h2>
                     <div class="widget-content">
@@ -126,7 +103,6 @@
                         </p>
                     </div>
                 </div><!-- Price-->
-
                 <div class="widget mercado-widget filter-widget">
                     <h2 class="widget-title">Color</h2>
                     <div class="widget-content">
@@ -140,7 +116,6 @@
                         </ul>
                     </div>
                 </div><!-- Color -->
-
                 <div class="widget mercado-widget filter-widget">
                     <h2 class="widget-title">Size</h2>
                     <div class="widget-content">
@@ -155,7 +130,6 @@
                         </div>
                     </div>
                 </div><!-- Size -->
-
                 <div class="widget mercado-widget widget-product">
                     <h2 class="widget-title">Popular Products</h2>
                     <div class="widget-content">
@@ -173,7 +147,6 @@
                                     </div>
                                 </div>
                             </li>
-
                             <li class="product-item">
                                 <div class="product product-widget-style">
                                     <div class="thumbnnail">
@@ -187,7 +160,6 @@
                                     </div>
                                 </div>
                             </li>
-
                             <li class="product-item">
                                 <div class="product product-widget-style">
                                     <div class="thumbnnail">
@@ -201,7 +173,6 @@
                                     </div>
                                 </div>
                             </li>
-
                             <li class="product-item">
                                 <div class="product product-widget-style">
                                     <div class="thumbnnail">
@@ -215,15 +186,13 @@
                                     </div>
                                 </div>
                             </li>
-
                         </ul>
                     </div>
                 </div><!-- brand widget-->
-
-            </div><!--end sitebar-->
-
-        </div><!--end row-->
-
-    </div><!--end container-->
-
+            </div>
+            <!--end sitebar-->
+        </div>
+        <!--end row-->
+    </div>
+    <!--end container-->
 </main>
