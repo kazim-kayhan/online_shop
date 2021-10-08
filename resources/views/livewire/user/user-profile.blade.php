877d156586cfafd4b1644296c75348f1cb4056fe
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2021-10-08 22:12:24 +0430

    Add editProfile link

--- resources/views/livewire/user/user-profile.blade.php
+++ resources/views/livewire/user/user-profile.blade.php
@@ -1,28 +1,22 @@
 <div>
     <div class="container">
         <div class="row">
-            <div class="pane panel-default">
-                Profile
-            </div>
+            <div class="pane panel-default"> Profile </div>
             <div class="panel-body">
-                <div class="col-md-4">
-                    @if($user->profile->image)
-                        <img src="{{ asset('assets/images/profile') }}/{{ $user->profile->image }}" alt="{{ $user->name }} photo" width="100%">
-                    @else
-                        <img src="{{ asset('assets/images/profile/default.png') }}" alt="{{ $user->name }} photo" width="100%">
-                    @endif
+                <div class="col-md-4"> @if($user->profile->image) <img src="{{ asset('assets/images/profile') }}/{{ $user->profile->image }}" alt="{{ $user->name }} photo" width="100%"> @else <img src="{{ asset('assets/images/profile/default.png') }}" alt="{{ $user->name }} photo" width="100%"> @endif </div>
+                <div class="col-md-8">
+                    <p><b>Name: </b>{{ $user->name }}</p>
+                    <p><b>Email: </b>{{ $user->email }}</p>
+                    <p><b>Phone: </b>{{ $user->profile->mobile }}</p>
+                    <hr>
+                    <p><b>Line1: </b>{{ $user->profile->line1 }}</p>
+                    <p><b>Line2: </b>{{ $user->profile->line2 }}</p>
+                    <p><b>City: </b>{{ $user->profile->city }}</p>
+                    <p><b>Province: </b>{{ $user->profile->province }}</p>
+                    <p><b>Country: </b>{{ $user->profile->country }}</p>
+                    <p><b>Zip Code: </b>{{ $user->profile->zipcode }}</p>
+                    <a href="{{ route('user.editProfile') }}" class="btn btn-info pull-right">Update Profile</a>
                 </div>
-                <div class="col-md-8"></div>
-                <p><b>Name: </b>{{ $user->name }}</p>
-                <p><b>Email: </b>{{ $user->email }}</p>
-                <p><b>Phone: </b>{{ $user->profile->mobile }}</p>
-                <hr>
-                <p><b>Line1: </b>{{ $user->profile->line1 }}</p>
-                <p><b>Line2: </b>{{ $user->profile->line2 }}</p>
-                <p><b>City: </b>{{ $user->profile->city }}</p>
-                <p><b>Province: </b>{{ $user->profile->province }}</p>
-                <p><b>Country: </b>{{ $user->profile->country }}</p>
-                <p><b>Zip Code: </b>{{ $user->profile->zipcode }}</p>
             </div>
         </div>
     </div>
